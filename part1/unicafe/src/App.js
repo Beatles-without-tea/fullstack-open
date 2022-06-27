4b2d9620b27bd36240e26f15e970af6261896ee6
Max <max.beales@gmail.com>
2022-06-27 18:18:20 +0200

    1.10: unicafe step5 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,7 +13,11 @@ const Button = (props) => {
     )
 }
 
-
+const StatisticLine = ({text,value}) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
 
 const Statistics = ({good, neutral, bad}) => {
   const average =  (good -bad)/ (good +neutral +bad)
@@ -26,12 +30,12 @@ const Statistics = ({good, neutral, bad}) => {
   }
   return (
     <div>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-    <p>all {sum}</p>
-    <p>average {average}</p>
-    <p>positive {positive} % </p>
+    <StatisticLine text='good' value={good}/>
+    <StatisticLine text='neutral' value={neutral}/>
+    <StatisticLine text='bad' value={bad}/>
+    <StatisticLine text='all' value={sum}/>
+    <StatisticLine text='average' value={average}/>
+    <StatisticLine text='positive' value={positive}/>
     </div>
   )
 }
