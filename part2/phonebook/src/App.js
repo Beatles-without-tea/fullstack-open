367c4e38c1019adc8664af53661f077bf7dc5a29
Max <max.beales@gmail.com>
2022-06-29 18:48:34 +0200

    2.9*: The Phonebook Step4 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,25 @@
 import { useState } from 'react'
 
 
+
+const FindMatches = ({persons,newSearch}) => {
+  const searchMatches = persons.filter( function(person) {
+    return person.name.includes(newSearch)})
+    return(
+    searchMatches.map(person => <p key={person.id}> {person.name} {person.number} </p>)
+    )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -17,7 +30,8 @@ const App = () => {
   }else{
     const nameObject ={
       name: newName , 
-      number: newNumber
+      number: newNumber,
+      id : persons.map(person => person.id)[persons.length-1]+1
     }
     
     setPersons(persons.concat(nameObject))
@@ -35,10 +49,18 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setNewSearch(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with: <input value={newSearch} onChange={handleSearchChange}/>
+        </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -51,7 +73,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}> {person.name} {person.number} </p>)}
+      < FindMatches persons={persons} newSearch={newSearch} />
     </div>
   )
 }
