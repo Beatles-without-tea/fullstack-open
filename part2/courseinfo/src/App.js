c1df1ad486524888ef49a5532524ad39460e6fb3
Max <max.beales@gmail.com>
2022-06-29 13:17:03 +0200

    2.2: Course information step7 done

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,6 +5,11 @@ const Course = ({course}) => {
     <div>
       <h1>{course.name}</h1>
       {course.parts.map(part => <p key={part.id}> {part.name} {part.exercises} </p>)}
+      
+      <b> total of {course.parts.map(part => part.exercises).reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      0
+    )} exercices </b>
     </div>
 
   )
