e2edcaf1d2595fa0a935f0dc708e1dec2649d8ca
Max <max.beales@gmail.com>
2022-07-01 15:46:17 +0200

    2.13*: Data for countries, step2 basic functions

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -4,13 +4,52 @@ import axios from 'axios'
 const FindCountries = ({countryData,newSearchCountry,handleCountryChange}) =>{
   return(
     <div>
-      find countries<input value={newSearchCountry} onChange={handleCountryChange}/>
-      
+      find countries<input value={newSearchCountry} onChange={handleCountryChange}/>   
     </div>
   )
 }
 
-const DisplayMatches = ({countryData,newSearchCountry}) => {
+const DisplayDetails = ({match}) => {
+  const languages = match.languages
+  return(
+    <div>
+    <h1>{match.name.common}</h1>
+    <p>capital {match.capital}</p>
+    <p>area {match.area}</p>
+    <b>languages:</b>
+    <ul>
+      {Object.values(languages).map(language => <li key={language}>{language}</li>)}
+    </ul>
+    <img src={match.flags.svg} width="300" height="250"/>
+    </div>
+  )
+}
+
+const DisplayCountrywithButton = ({matchingCountries,handleButtonPress,extraInfo}) => {
+  // if (extraInfo[country.name.common]){
+  //   < DisplayDetails match={country}/>
+  // }
+
+  return(
+    
+    matchingCountries.map(country => 
+    <div>
+    <table>
+    <tr>
+      <td><p key={country.name.common}>{country.name.common}</p></td>
+      
+     <td> <button onClick={(event) =>handleButtonPress(event,country)}>show</button></td>
+    </tr>
+  </table>
+  {extraInfo[country.name.common] ? < DisplayDetails match={country}/>:""}
+
+  {console.log(extraInfo)}
+    </div>
+  )
+  )
+}
+
+const DisplayMatches = ({countryData,newSearchCountry,handleButtonPress,extraInfo}) => {
   const matchingCountries = countryData.filter( function(country){
     return country.name.common.toLowerCase().includes(newSearchCountry.toLowerCase())})
   const matchingNames=matchingCountries.map(country => country.name.common)
@@ -20,24 +59,12 @@ const DisplayMatches = ({countryData,newSearchCountry}) => {
     )
   }else if ((matchingNames.length<=10) && (matchingNames.length>1)){
     return (
-    matchingNames.map(name => <p key={name}>{name}</p>)
+     < DisplayCountrywithButton extraInfo={extraInfo} matchingNames={matchingNames} matchingCountries={matchingCountries} handleButtonPress={handleButtonPress}/>
     )
   }else if (matchingNames.length==1){
     const match = matchingCountries[0]
-    const languages = match.languages
     return(
-      <div>
-      <h1>{match.name.common}</h1>
-      <p>capital {match.capital}</p>
-      <p>area {match.area}</p>
-      <b>languages:</b>
-      
-      <ul>
-        {Object.values(languages).map(language => <li key={language}>{language}</li>)}
-      </ul>
-      <img src={match.flags.svg} width="300" height="250"/>
-      </div>
-
+    <DisplayDetails match={match}/>
     )
   }
   } 
@@ -45,6 +72,7 @@ const DisplayMatches = ({countryData,newSearchCountry}) => {
 const App = () => {
   const [newSearchCountry, setSearchCountry] = useState('')
   const [countryData ,setCountryData] = useState([])
+  const [extraInfo, setExtraInfo] = useState({'placeholder':true})
 
   const hook = () => {
     console.log('effect')
@@ -64,6 +92,19 @@ const App = () => {
     setSearchCountry(event.target.value)
   }
 
+  const handleButtonPress = (event,country,extraInfo) => {
+    console.log('event target',event.target)
+    const country_name=country.name.common
+    console.log('country',country_name)
+    
+
+    setExtraInfo({...extraInfo, [country_name]: true})
+    
+    console.log('button pressed')
+    console.log('extrainfo', extraInfo)
+
+    
+  }
 
   
 
@@ -72,7 +113,7 @@ const App = () => {
     <div>
 
     <FindCountries newSearchCountry={newSearchCountry} handleCountryChange={handleCountryChange} />
-    <DisplayMatches countryData={countryData} newSearchCountry={newSearchCountry}/>
+    <DisplayMatches countryData={countryData} extraInfo={extraInfo} newSearchCountry={newSearchCountry} handleButtonPress={handleButtonPress}/>
     </div>
 
   )
