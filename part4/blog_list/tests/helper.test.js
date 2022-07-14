6455063f0c31741ab382754529fc4b41c58617bc
Max <max.beales@gmail.com>
2022-07-14 15:55:46 +0200

    4.4: helper functions and unit tests, step2 done

--- part4/blog_list/tests/helper.test.js
+++ part4/blog_list/tests/helper.test.js
@@ -1,8 +1,65 @@
 const listHelper = require('../utils/list_helper')
 
+
 test('dummy returns one',() => {
     const blogs = []
 
     const result = listHelper.dummy(blogs)
     expect(result).toBe(1)
+    })
+
+describe('total likes', () => {
+    const blogs = [
+        {
+            "_id": "62cfde495c792e209c7a9d40",
+            "title": "The wonderfulworld of Superdogman",
+            "author": "max the magnanimous",
+            "url": "www.idk.com",
+            "likes": 500000
+            },
+            {
+            "_id": "62cfdf62d72936fc69edd387",
+            "title": "super dogman",
+            "author": "09678978393",
+            "url": "www.test_link.com",
+            "likes": 10000,
+            "__v": 0
+            },
+            {
+                "_id": "62d01e6dc88f448f62f286f9",
+                "title": "super cat",
+                "author": "max",
+                "url": "www.test_link.com",
+                "likes": 3,
+                "__v": 0
+              },
+              {
+                "_id": "62d01e86c88f448f62f286fb",
+                "title": "words for wisdom",
+                "author": "john",
+                "url": "www.test_link.com",
+                "likes": 10,
+                "__v": 0
+              }
+    ]
+    test('totalLikes returns sum of likes', () => {
+        const result = listHelper.totalLikes(blogs)
+        expect(result).toBe(510013)
+    })
+    test('totalLikes with only one blog', () => {
+        const result = listHelper.totalLikes(blogs[0])
+        expect(result).toBe(500000)
+    })
+    test('totalLikes with only one blog', () => {
+        const result = listHelper.totalLikes(blogs[1])
+        expect(result).toBe(10000)
+    })
+    test('totalLikes with only one blog', () => {
+        const result = listHelper.totalLikes(blogs[2])
+        expect(result).toBe(3)
+    })
+    test('totalLikes with empty array', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
 })
--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,22 @@
+const blog = require("../models/blog");
+
 const dummy = (blogs) =>{
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let counter = 0;
+    if (blogs.length>1){
+        blogs.forEach(blog => counter+=blog.likes);
+    }else if (blogs.length==0){
+        return 0
+    }else {
+        counter+=blogs.likes
+    }
+    return counter
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
