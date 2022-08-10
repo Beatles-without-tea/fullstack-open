0bcb8d08e066d3d06b570f4ec911bc9cba430e56
Max <max.beales@gmail.com>
2022-08-10 15:11:30 +0200

    4.18: bloglist expansion, step6 done

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
-
+const loginRouter = require('./controllers/login')
 
 const blogsRouter = require('./controllers/blog')
 const {MONGODB_URI, PORT} = require('./utils/config')
@@ -23,6 +23,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
-
+app.use('/api/login', loginRouter)
 
 module.exports = app
